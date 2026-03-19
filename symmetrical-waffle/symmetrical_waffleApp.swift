42bb417f1c9eecfe04f0ce289a5bf7caa794ef79
kartikayy007 <kartikayy6969@gmail.com>
2026-03-19 19:20:00 +0530

    Initial Xcode project setup

---
+++ symmetrical-waffle/symmetrical_waffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  symmetrical_waffleApp.swift
+//  symmetrical-waffle
+//
+//  Created by kartikay on 19/03/26.
+//
+
+import SwiftUI
+
+@main
+struct symmetrical_waffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
